b4a6910e1a93fb338508a6c22e24ffecbb2a9dec
cscal <cscal@protonmail.com>
2019-07-24 22:25:06 -0400

    more work on pxe

--- roles/pxe/tasks/dhcpd.sh
+++ roles/pxe/tasks/dhcpd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 #Performing clean up of any actions and exit with successful return status
 clean_exit()
 {
@@ -15,8 +16,9 @@ echo "$PROG: sorry dawg yall aint root" >> ${LOGFILE} 2>&1
 exit 1
 fi
 
-#Transforming Skellig DHCP energy into Dologruin transfitia automata
+sudo yum install -y sshpass;
 
+#Transforming Skellig DHCP energy into Dologruin transfitia automata
 s=subnet;
 rr=range;
 odns=option domain-name-servers;
@@ -48,9 +50,103 @@ default-lease-time 600;
 max-lease-time 7200;
 
 # IP of PXE Server
-next-server 192.168.1.6;
+next-server 192.168.45.15;
 filename "pxelinux.0";
 }
 EOF
 
-sed -i 's/no/yes' /etc/xinetd.d/tftp
+echo "DHCPDARGS=ens192" >> /etc/sysconfig/dhcpd
+sudo sed -i 's/no/yes' /etc/xinetd.d/tftp;
+sudo cp -vp /usr/share/syslinux/{pxelinux.0,menu.c32,memdisk,mboot.c32,chain.c32} /var/lib/tftpboot;
+sudo mkdir /var/lib/tftpboot/pxelinux.0 && sudo mkdir /var/lib/tftpboot/networkboot;
+sudo mount -o loop ftp://192.168.45.17/pub/CentOS7/CentOS-7-x86_64-DVD-1810.iso /mnt/;
+sudo umount /mnt/;
+sudo openssl passwd -1 password >> passgen;
+sudo var=passgen;
+sudo cat << EOF > centos7.cfg
+#version=DEVEL
+
+#Firewall configuration
+firewall --disabled
+
+# Install OS instead of upgrade install
+install 
+
+# Use FTP installation media
+url --url="ftp://192.168.45.17/pub/CentOS7/"
+
+# Root password
+rootpw --iscrypted $var
+
+# System authorization information
+auth useshadow passalgo=sha512
+
+# Use graphical install graphical
+firstboot disable
+
+#System keyboard
+keyboard us
+
+#System language
+lang en_US
+
+#SELinux configuration
+selinux disabled
+
+#Installation logging level
+logging level=info
+
+#System timezone 
+timezone "America/New_York"
+
+#System bootloader configuration
+bootloader location=mbr
+clearpart --all --initlabel
+autopart --type=lvm
+
+#System Services
+services --enabled="chronyd"
+
+#Packages to install
+%packages
+@^minimal
+@core
+chrony
+wget
+ntp
+mlocate
+bind-utils
+sysstat
+man
+man-pages
+%end
+
+%addon com_redhat_kdump --disable --reserve-mb='auto'
+%end
+EOF
+
+
+cat << EOF > /var/lib/tftpboot/pxelinux.cfg/default
+default menu.c32
+prompt 0
+timeout 30
+MENU TITLE ######## PXE Menu #########
+LABEL 1
+MENU LABEL ^1) Install CentOS 7
+KERNEL /networkboot/vmlinuz
+APPEND initrd=/networkboot/initrd.img inst.repo=ftp://192.168.45.17/pub/CentOS7/ ks=ftp://192.168.45.17/pub/CentOS7/centos7.cfg
+EOF
+
+systemctl status firewalld | grep -i active | awk '{print $2;}' > status;
+stat=status
+if [ "$stat" == "active" ]; then
+  sudo systemctl start xinetd && sudo systemctl start dhcpd && sudo systemctl start vsftpd && sudo systemctl enable xinetd && sudo systemctl enable dhcpd && sudo systemctl enable vsftpd;
+elif [ "$stat" == "inactive" ]; then
+  echo "firewalld is not rinning";
+else
+  echo "firewall doesn't need to be configured";
+fi
+
+exit 0;
+
+clean_exit
